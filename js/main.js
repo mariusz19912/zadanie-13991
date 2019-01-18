432336e2e440a6b904fc66b73b50199d37131327
Mariusz Betkier <mariusz.betkier11@gmail.com>
2019-01-18 20:30:36 +0100

    zadanie nie działa do końca

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const string1 = "Uwielbiam JavaScript",
+      string2 = "Jestem świetnym programistą";
+
+function porownanie(string1,string2) {
+  if (string1<string2) {
+    console.log(string2);
+  } 
+};
+porownanie(string1,string2);
